1901bd3175fe34cead37dbccbe0647dbc3a16234
Matt Gilliland <mgilliland23@gmail.com>
2019-09-13 06:41:16 -0400

    added price to card

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -7,10 +7,7 @@ function Card(props) {
       <img alt={props.name} src={props.image} className="card-img-top" />
       <div className="card-body">
         <h5 className="card-title">{props.name}</h5>
-        {/* <p className="card-text">
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </p> */}
+        <p className="card-text">${props.price}</p>
         <button
           className="btn btn-custom"
           onClick={() => props.handleClick(props.name)}
